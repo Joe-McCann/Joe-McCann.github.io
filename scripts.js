1415b6c07a0dee4987a0ae24b89b4559db0d045b
Joe McCann <wjm9@njit.edu>
2017-07-18 21:22:50 -0400

    .2
    Added projects and images, as well as a slide show through my projects.

---
+++ scripts.js
@@ -0,0 +1,31 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("project");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    //x[slideIndex-1].style.display = "block";
+    unfade(x[slideIndex-1])
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 50);
+}
